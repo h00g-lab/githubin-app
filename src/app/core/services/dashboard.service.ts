571851282c7a14ff26e84a7f9ac8cb21bb3a3a9a
Fernando Valle <fernando.valle@performait.com>
2022-06-17 21:22:55 -0300

    Add url in environment

--- src/app/core/services/dashboard.service.ts
+++ src/app/core/services/dashboard.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DashboardService {
+    private apiUrl: string = `${environment.apiUrl}`
 
-    constructor() { }
+    constructor(
+		private http: HttpClient
+	) { }
+	public get(username: string, repository: string): Observable<any> {
+		let url = `${this.apiUrl}/${username}/${repository}`;
+		return this.http.get<any>(url);
+	}
 }
